969ff30c148b0e46183c3b2827f6bcc284acb5b0
Cory Spicer <corys4139@gmail.com>
2020-07-16 17:02:18 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,11 @@
 def turn_count(board)
   counter = 0
-  board.each do |space|
-    if space == "X" || space == "O"
+  board.each do |turn|
+    if turn == "X" || turn == "O"
       counter += 1
     end
   end
-  return turns
+  return counter
 end
 
 def current_player(board)
